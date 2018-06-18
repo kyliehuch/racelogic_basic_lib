7b9633c9410a1259ea3c4d8bde83d773460e490a
kyliehuch <kyliehuch@umail.ucsb.edu>
2018-06-18 15:50:17 -0700

    Fixed max() and min() in added functions to be rmax() and rmin() respectively, also added tests

--- racelogic.py
+++ racelogic.py
@@ -1,4 +1,3 @@
-import pyrtl
 from functools import reduce
 
 class Infinity:
@@ -8,7 +7,7 @@ inf = Infinity() # this value will serve as our infinity
 
 
 def rmin(a,b):
-	''' min function '''
+    ''' min function '''
     if a==inf:
         return b
     elif b==inf:
@@ -16,20 +15,20 @@ def rmin(a,b):
     return min(a,b)
 
 def rmax(a,b):
-	''' max function '''
+    ''' max function '''
     if a==inf or b==inf:
         return inf
     return max(a,b)
 
 def rd(a,c=1):
-	''' delay function '''
+    ''' delay function '''
     if a==inf or c==inf:
         return inf
     return a+c
 
 def ri(i,x):
-	''' inhibit function '''
-	if i==inf:
+    ''' inhibit function '''
+    if i==inf:
         return x
     elif x==inf:
         return inf
@@ -37,14 +36,60 @@ def ri(i,x):
         return inf
     return x
 
+# delays signal a by time c if inhibit signal i arives before a
+#   if (i<=a) return a+c , else (i>a) return a
 def rvd(a,c,i):
     ''' variable delay function '''
-    return min( (rd(a,c)), (ri(i,a)) )
+    delayed_signal = rd(a,c)
+    inhibited_signal = ri(i,a)
+    return rmin(delayed_signal, inhibited_signal)
 
+# returns max(a,b) if a and b arive within fixed time c of eachother
+#   if (abs(a-b) < c) return rmax(a,b), else (abs(a-b) >= c) return -
 def rcoincidence(a,b,c):
     ''' timed max function '''
-    return ri( (rd(min(a,b),c)), (max(a,b)) )
+    timer = rd(rmin(a,b),c)
+    return ri(timer, rmax(a,b))
 
+# returns max(a,b) if a and b arive within variable time c of eachother
+#   delay is 'on' if i arives before the first of the two values; i <= rmin(a,b)
 def rvcoincidence(a,b,c,i):
     ''' variable timed max function '''
-    return ri( (rvd(min(a,b),c,i)), (max(a,b)) )
+    variable_timer = rvd(rmin(a,b),c,i)
+    return ri(variable_timer, rmax(a,b))
+
+# tests:
+print("rmin(inf,5) = ", end="")
+print(rmin(inf,5))
+print("rd(5,2) = ", end="")
+print(rd(5,2))
+print("ri(5,2) = ", end="")
+print(ri(5,2))
+print("ri(2,5) = ", end="")
+print(ri(2,5))
+print("rvd(5,2,8) = ", end="")
+print(rvd(5,2,8))
+print("rvd(5,2,3) = ", end="")
+print(rvd(5,2,3))
+print("rvd(5,2,7) = ", end="")
+print(rvd(5,2,7))
+print("rvd(5,2,6) = ", end="")
+print(rvd(5,2,6))
+print("rvd(5,2,5) = ", end="")
+print(rvd(5,2,5))
+print("rcoincidence(5,2,1) = ", end="")
+print(rcoincidence(5,2,1))
+print("rcoincidence(5,2,4) = ", end="")
+print(rcoincidence(5,2,4))
+print("rcoincidence(5,2,3) = ", end="")
+print(rcoincidence(5,2,3))
+print("rcoincidence(5,2,3.1) = ", end="")
+print(rcoincidence(5,2,3.1))
+print("rvcoincidence(5,2,4,3) = ", end="")
+print(rvcoincidence(5,2,4,3)) # delay off
+print("rvcoincidence(5,2,4,1) = ", end="")
+print(rvcoincidence(5,2,4,1)) # delay on
+print("rvcoincidence(5,2,1,3) = ", end="")
+print(rvcoincidence(5,2,1,3)) # delay off
+print("rvcoincidence(5,2,1,1) = ", end="")
+print(rvcoincidence(5,2,1,1)) # delay on
