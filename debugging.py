2925385be2260493d39f9aa4f3072138c36bcb35
kyliehuch <kyliehuch@umail.ucsb.edu>
2018-06-20 15:27:05 -0700

    More debugging functions

--- debugging.py
+++ debugging.py
@@ -67,3 +67,39 @@ in1, in2 = (pyrtl.Input(8, "in" + str(x)) for x in range(1, 3))
 out1, out2 = (pyrtl.Output(8, "out" + str(x)) for x in range(1, 3))
 
 multout = multipliers.tree_multiplier(in1, in2)
+
+# The following line will create a probe named 'std_probe" for later use, like an output.
+pyrtl.probe(multout, 'std_probe')
+
+# The probe returns multout, the original wire, and multout * 2 will be assigned to out1
+out1 <<= pyrtl.probe(multout, 'stdout_probe') * 2
+
+# probe can also be used with other operations like this:
+pyrtl.probe(multout + 32, 'adder_probe')
+
+# or this:
+pyrtl.probe(multout[2:7], 'select_probe')
+
+# or, similarly:
+# (this will create a probe of multout while passing multout[2:16] to out2)
+out2 <<= pyrtl.probe(multout)[2:16]
+
+# probe can be used on any wire any time, even before or during its operation, assignment, etc.
+
+# Now on to the simulation...
+# For variation, we'll recreate the random inputs:
+vals1 = [int(2**random.uniform(1, 8) - 2) for _ in range(10)]
+vals2 = [int(2**random.uniform(1, 8) - 2) for _ in range(10)]
+
+sim_trace = pyrtl.SimulationTrace()
+sim = pyrtl.Simulation(tracer=sim_trace)
+for cycle in range(len(vals1)):
+    sim.step({
+        'in1': vals1[cycle],
+        'in2': vals2[cycle]})
+
+# Now we will show the values of the inputs and probes
+# and look at that, we didn't need to make any outputs!
+# (although we did, to demonstrate the power and convenience of probes)
+sim_trace.render_trace()
+sim_trace.print_trace()
