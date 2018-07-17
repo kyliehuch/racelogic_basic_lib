f35dcd1d3360a0c1057b2589edac917ef4e7f3e5
kyliehuch <kyliehuch@umail.ucsb.edu>
2018-07-17 14:35:11 -0700

    Implemented full coincidence gate

--- test.py
+++ test.py
@@ -1,17 +1,21 @@
 import itertools
 import pyrtl
 
-'''
-in1, in2, in3 = (pyrtl.Input(1, "in" + str(x)) for x in range(1,4))
+def test(*in_list):
+    return pyrtl.rtl_any(*in_list)
+
+
+in1, in2, in3, in4, in5 = (pyrtl.Input(1, "in" + str(x)) for x in range(1,6))
 out = pyrtl.Output(1, "out")
 
-input = [in1,in2,in3]
+out <<= test(in1,in2,in3,in4,in5)
 
-out <<= pyrtl.rtl_any()
 
-in1_vals = [0,0,1,1,1,1]
-in2_vals = [0,0,0,0,1,1]
-in3_vals = [0,0,0,1,1,1]
+in1_vals = [0,0,1,1,1,1,1,1]
+in2_vals = [0,0,0,0,1,1,1,1]
+in3_vals = [0,0,0,1,1,1,1,1]
+in4_vals = [0,0,0,0,0,0,1,1]
+in5_vals = [0,0,0,0,0,1,1,1]
 
 sim_trace = pyrtl.SimulationTrace()
 sim = pyrtl.Simulation(tracer=sim_trace)
@@ -19,21 +23,8 @@ for cycle in range(len(in1_vals)):
     sim.step({
         'in1': in1_vals[cycle],
         'in2': in2_vals[cycle],
-        'in3': in3_vals[cycle]})
+        'in3': in3_vals[cycle],
+        'in4': in4_vals[cycle],
+        'in5': in5_vals[cycle]})
 
 sim_trace.render_trace()
-'''
-
-in_list = [1,1,0]
-
-combo_list = list(itertools.combinations(in_list,2))
-print(combo_list)
-prod_list = [(x[0] & x[1]) for x in combo_list]
-print(prod_list)
-
-'''
-def vmax(c,*args):
-    wires = [("tmp" + str(x)) for x in range(len(args[0]))]
-    for i in range(0,len(args[0])):
-        wires[i] = args[0][i]
-'''
--- vmax.py
+++ vmax.py
@@ -2,19 +2,26 @@ import pyrtl
 import itertools
 
 # --------- Function ------------
+
+# takes list of wirevectors and number of signals required to 'fire' (refered to
+# fire as "threshold") as inputs
 def vmax(t,*in_list):
     combo_list = list(itertools.combinations(in_list,t))
     prod_list = [pyrtl.rtl_all(*x) for x in combo_list]
     o = pyrtl.rtl_any(*prod_list)
     return o
 
+
 # --------- Hardware -------------
+
 in1, in2, in3, in4, in5 = (pyrtl.Input(1, "in" + str(x)) for x in range(1,6))
 out = pyrtl.Output(1, "out")
 
 out <<= vmax(5,in1,in2,in3,in4,in5)
 
+
 # --------- Hardcoded Solution ---------
+
 '''
 in_list = [in1, in2, in3]
 combo_list = list(itertools.combinations(in_list,2))
@@ -23,7 +30,9 @@ prod_list = [(x[0] & x[1]) for x in combo_list]
 out <<= pyrtl.rtl_any(*prod_list)
 '''
 
+
 # --------- Simulation ------------
+
 in1_vals = [0,0,1,1,1,1,1,1]
 in2_vals = [0,0,0,0,1,1,1,1]
 in3_vals = [0,0,0,1,1,1,1,1]
