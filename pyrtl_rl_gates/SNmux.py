7d63ed564f107cafca2e96ac9c9a3630b2b34224
kyliehuch <kyliehuch@umail.ucsb.edu>
2019-03-01 14:48:50 -0800

    Implemented SNmux gate

--- pyrtl_rl_gates/SNmux.py
+++ pyrtl_rl_gates/SNmux.py
@@ -45,30 +45,29 @@ def sn(a,b,c,d):
     o3 = rmax(i2,i3)
     return (o1,o2,o3,o4)
 
+# n = timer start index (param for upper mux)
+# m = threshold index (param for lower mux)
+# d = delay on inhibit (timer)
+def SNmux(d,n,m,a1,a2,a3,a4):
+    outs = sn(in1,in2,in3,in4)
+    temp1 = pyrtl.mux(n,outs[0],outs[1],outs[2],outs[3])
+    temp2 = rdelta(2,temp1)
+    temp3 = pyrtl.mux(m,outs[0],outs[1],outs[2],outs[3])
+    return rinhibit(temp2,temp3)
+
+
 #---------- Hardware ---------
 
 in1, in2, in3, in4 = (pyrtl.Input(1, "in" + str(x)) for x in range(1,5))
-o1, o2, o3, o4 = (pyrtl.WireVector(1, "o" + str(x)) for x in range(1,5))
 out = pyrtl.Output(1, "out")
-temp1, temp2, temp3 = (pyrtl.WireVector(1, "temp" + str(x)) for x in range(1,4))
 n, m = pyrtl.Const(1,2), pyrtl.Const(2,2)
 
-
-outs = sn(in1,in2,in3,in4)
-o1 <<= outs[0]
-o2 <<= outs[1]
-o3 <<= outs[2]
-o4 <<= outs[3]
-
-temp1 <<= pyrtl.mux(n,o1,o2,o3,o4)
-temp2 <<= rdelta(2,temp1)
-temp3 <<= pyrtl.mux(m,o1,o2,o3,o4)
-out <<= rinhibit(temp2,temp3)
+out <<= SNmux(2,n,m,in1,in2,in3,in4)
 
 #--------- Simulation ---------
 
 in1_vals = [0,0,1,1,1,1,1,1]
-in2_vals = [0,0,0,0,0,1,1,1]
+in2_vals = [0,0,0,0,1,1,1,1]
 in3_vals = [0,1,1,1,1,1,1,1]
 in4_vals = [0,0,0,0,0,0,1,1]
 
